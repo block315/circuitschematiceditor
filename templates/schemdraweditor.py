99943c56727c8c5e35c141a1124715b14f9b5946
block315 <block315ccc@gmail.com>
2025-12-29 16:38:16 +0900

    fix: add import statement for digital logic in template file

--- templates/schemdraweditor.py
+++ templates/schemdraweditor.py
@@ -1,5 +1,8 @@
 import schemdraw
 from schemdraw.elements import *
+import schemdraw.elements as elm
+from schemdraw import logic
+from schemdraw.logic import *
 
 schemdraw.theme('[% theme %]')
 with schemdraw.Drawing(show=False, file='[% circuit_svg_fname %]'):
